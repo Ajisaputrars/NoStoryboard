6066f7006924fe89b394cde515ed9277d3f5a7a3
Aji Saputra Raka Siwi <ajisaputrars@gmail.com>
2020-05-09 14:36:06 +0700

    Initial Commit

---
+++ SampleProjectWithoutStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProjectWithoutStoryboard
+//
+//  Created by Aji Saputra Raka Siwi on 09/05/20.
+//  Copyright © 2020 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
